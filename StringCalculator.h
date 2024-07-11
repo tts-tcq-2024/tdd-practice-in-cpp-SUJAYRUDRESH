583e070991d8ce26fa29366175a1f8af1f759668
SUJAYRUDRESH <60745093+SUJAYRUDRESH@users.noreply.github.com>
2024-07-11 23:37:38 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -16,6 +16,8 @@ private:
     static int AddNumber(int sum, const std::string& number);
     static int SumNumbers(const std::string& numbers);
     static bool ShouldIgnoreNumber(const std::string& number);
+    static void CollectNegativeNumber(const std::string& number, std::string& negatives);
+    static void CheckForNegatives(const std::string& numbers, std::string& negatives);
 };
 
 #endif // STRING_CALCULATOR_HPP
